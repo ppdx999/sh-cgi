d1d0be7eb330f0af5d0eca27ad526e88fef04497
ppdx999 <ppdx999@gmail.com>
2024-01-30 22:07:41 +0900

    ログイン中かそうでないかによって画面表示を切り替える

--- public/index.sh
+++ public/index.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+# Login check
+is_login="no"
+session-check "${HTTP_COOKIE:-}"
+case $? in 0) is_login="yes";; esac
+
+status="Logout"
+case $IS_LOGIN in "yes") status="Login";; *) status="Logout";; esac
+
 echo "Content-Type: text/html"
 echo ""
-cat index.template.html
+cat index.template.html | sed -e "s/@@STATUS@@/$status/g"
